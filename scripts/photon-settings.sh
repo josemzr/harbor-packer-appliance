30df3cf9df392952e8bf93f841e54bd29d4c297c
Jose Manzaneque Ramos <vaulner@gmail.com>
2022-06-30 09:01:57 +0200

    Update photon-settings.sh

--- scripts/photon-settings.sh
+++ scripts/photon-settings.sh
@@ -7,9 +7,6 @@
 echo '> Disable IPv6'
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 
-echo '> Update package repos to VMware'
-sed  -i 's/dl.bintray.com\/vmware/packages.vmware.com\/photon\/$releasever/g' /etc/yum.repos.d/photon.repo /etc/yum.repos.d/photon-updates.repo /etc/yum.repos.d/photon-extras.repo /etc/yum.repos.d/photon-debuginfo.repo
-
 echo '> Applying latest Updates...'
 tdnf -y update
 
